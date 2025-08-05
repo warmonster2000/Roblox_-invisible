b50a89716cc0a4e44c83f6a4bb93362f799957f1
warmonster2000 <tiktoktut938@gmail.com>
2025-08-05 23:17:44 +0300

    Create Help.lua

---
+++ Help.lua
@@ -0,0 +1,7 @@
+-- Для GitHub (замените username, repo и filepath)
+local githubUrl = "https://raw.githubusercontent.com/ВАШ_USERNAME/ВАШ_РЕПО/main/ВАШ_ФАЙЛ.lua"
+
+-- Для Pastebin (замените XXXXXXXX на ваш код)
+local pastebinUrl = "https://pastebin.com/raw/XXXXXXXX"
+
+loadstring(game:HttpGet(pastebinUrl, true))()
